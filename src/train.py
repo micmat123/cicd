1fd4e677bc482be5bae4cc99155a60cc1b9af4fa
michal <michal@mail.com>
2024-12-22 11:24:17 +0000

    Add logging to mlflow

--- src/train.py
+++ src/train.py
@@ -2,6 +2,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 import joblib
+import mlflow
+
+mlflow.set_tracking_uri('https://8a0b-34-28-92-193.ngrok-free.app')
 
 data = load_iris()
 X = data.data
@@ -14,6 +17,11 @@ X_train, X_test, y_train, y_test = train_test_split(X,
 
 lr = LogisticRegression()
 lr.fit(X_train, y_train)
-print(lr.score(X_test, y_test))
+score = lr.score(X_test, y_test)
+print(score)
+
+mlflow.set_experiment('cicd')
+with mlflow.start_run():
+    mlflow.log_metrics('score', score)
 
 joblib.dump(lr, "model.joblib")
